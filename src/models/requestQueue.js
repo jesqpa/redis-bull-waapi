86a6551fccd0e17b7079b587498f025c8df6773c
Jonathan Esquivel <jesqpa@gmail.com>
2025-04-03 17:17:56 -0600

    Segundo commit a railway

--- src/models/requestQueue.js
+++ src/models/requestQueue.js
@@ -2,6 +2,7 @@ const Queue = require("bull");
 
 const requestQueue = new Queue("requestQueue", {
     redis: { 
+        family: 0,
         host: process.env.REDIS_HOST, 
         port: process.env.REDIS_PORT, 
         password: process.env.REDIS_PASSWORD 
