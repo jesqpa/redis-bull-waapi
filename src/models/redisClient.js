25adce8a00f8b9ff388d70141b9aa854df6858f8
Jonathan Esquivel <jesqpa@gmail.com>
2025-04-03 17:09:51 -0600

    Segundo commit a railway

--- src/models/redisClient.js
+++ src/models/redisClient.js
@@ -1,11 +1,11 @@
 const Redis = require("ioredis");
 
-/*const redisClient = new Redis({
+const redisClient = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
-});*/
+});
 
-const redisClient = new Redis(/*process.env.REDIS_URL || */"redis://redis1.railway.internal:6379");
+//const redisClient = new Redis(process.env.REDIS_URL || "redis://redis1.railway.internal:6379");
 
 module.exports = redisClient;
